1c152c9ea1a7462c72a99134b5e98c45d1db5371
AshishS <ashish.sondagar@peopleinteractive.in>
2022-02-13 14:39:55 +0530

    update code

--- home/api_views/send_email_api.py
+++ home/api_views/send_email_api.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.http import HttpResponse
 from django.template.loader import render_to_string
+from home.cron_jobs.send_email_cron_job import send_email
 from rest_framework import routers, serializers, viewsets, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -12,32 +13,21 @@ import json, logging
 logger = logging.getLogger(__name__)
 
 
-@api_view(['POST'])
-def send_ex_email(request):
+@api_view(['GET'])
+def send_test_email(request):
     api_response = {'status_code': status.HTTP_500_INTERNAL_SERVER_ERROR, 'status': 'failed', 'messages': [], 'data': []}
     try:
         logger.info(f'---START---')
-        logger.debug(f'---request.data: {request.data}')
-        if 1:
-            email_subject = request.data.get('email', {}).get('subject', '')
-            email_body = request.data.get('email', {}).get('body', '')
-            to_email = request.data.get('email', {}).get('to_email', [])
-
-            datetime_util_obj = DateTimeUtil()
-            email_subject = f'{email_subject} - {datetime_util_obj.get_current_datetime()}'
-            # email_body = render_to_string('send_emails/ex_email.html', email_body)
-            email_body = render_to_string('send_emails/ex_detail_email.html', email_body)
-
-            email_service_obj = EmailService()
-            # email_service_obj.send_email()
-            email_service_obj.send_email_v2(email_subject, email_body, to_email)
+        res1 = send_email()
+        if res1:
             api_response['status_code'] = status.HTTP_200_OK
             api_response['status'] = 'success'
             api_response['messages'].append('email send successfully.')
             api_response['data'].append(request.data)
+
     except Exception as ex:
-        logger.critical('---send_ex_email---funn---EXCEPTION---', exc_info=True)
-        logger.critical(f'---send_ex_email---funn---EXCEPTION--msg: {ex}')
+        logger.critical('---send_form_email---funn---EXCEPTION---', exc_info=True)
+        logger.critical(f'---send_form_email---funn---EXCEPTION--msg: {ex}')
         api_response.update({'exception_message': [str(ex)]})
     finally:
         logger.info(f'---api_response: {api_response}')
@@ -74,6 +64,38 @@ def send_form_email(request):
     except Exception as ex:
         logger.critical('---send_form_email---funn---EXCEPTION---', exc_info=True)
         logger.critical(f'---send_form_email---funn---EXCEPTION--msg: {ex}')
+        api_response.update({'exception_message': [str(ex)]})
+    finally:
+        logger.info(f'---api_response: {api_response}')
+        return Response(api_response)
+
+
+@api_view(['POST'])
+def send_ex_email(request):
+    api_response = {'status_code': status.HTTP_500_INTERNAL_SERVER_ERROR, 'status': 'failed', 'messages': [], 'data': []}
+    try:
+        logger.info(f'---START---')
+        logger.debug(f'---request.data: {request.data}')
+        if 1:
+            email_subject = request.data.get('email', {}).get('subject', '')
+            email_body = request.data.get('email', {}).get('body', '')
+            to_email = request.data.get('email', {}).get('to_email', [])
+
+            datetime_util_obj = DateTimeUtil()
+            email_subject = f'{email_subject} - {datetime_util_obj.get_current_datetime()}'
+            # email_body = render_to_string('send_emails/ex_email.html', email_body)
+            email_body = render_to_string('send_emails/ex_detail_email.html', email_body)
+
+            email_service_obj = EmailService()
+            # email_service_obj.send_email()
+            email_service_obj.send_email_v2(email_subject, email_body, to_email)
+            api_response['status_code'] = status.HTTP_200_OK
+            api_response['status'] = 'success'
+            api_response['messages'].append('email send successfully.')
+            api_response['data'].append(request.data)
+    except Exception as ex:
+        logger.critical('---send_ex_email---funn---EXCEPTION---', exc_info=True)
+        logger.critical(f'---send_ex_email---funn---EXCEPTION--msg: {ex}')
         api_response.update({'exception_message': [str(ex)]})
     finally:
         logger.info(f'---api_response: {api_response}')
--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from home.api_views.send_email_api import send_ex_email, send_form_email
+from home.api_views.send_email_api import send_test_email, send_form_email, send_ex_email
 
 
 urlpatterns = [
-    path('api-send-ex-email/', send_ex_email, name='home-send-ex-email'),
+    path('send-test-email/', send_test_email, name='home-send-test-email'),
     path('api-send-form-email/', send_form_email, name='home-send-form-email'),
+    path('api-send-ex-email/', send_ex_email, name='home-send-ex-email'),
 ]
